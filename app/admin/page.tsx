06b81081aa1e4aa84881ba7ea1cf24d781ddb550
rohitpatel-dev <rohitrampatel185@gmail.com>
2025-11-01 14:32:50 +0530

    small bug fix

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,28 +1,11 @@
-// /app/dashboard/page.tsx (Server Component)
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
+import * as React from "react";
 
-export default async function DashboardPage() {
-  const session = await getServerSession(authOptions);
+const ComponentName = () => {
+  return (
+    <div>
+      <h1>hello Admin</h1>
+    </div>
+  );
+};
 
-  if (!session) {
-    return <div>You must be logged in.</div>;
-  }
-
-  if (session.user?.role != "admin") {
-    return (
-      <div>
-        <h1>You dont have required Permision to access this page</h1>
-      </div>
-    );
-  }
-
-  if (!session && session.user?.role === "admin")
-    return (
-      <div className="p-10">
-        <h1 className="text-2xl font-bold">Welcome, {session.user?.name}!</h1>
-        <p>Email: {session.user?.email}</p>
-        <p>Role: {session.user?.role}</p>
-      </div>
-    );
-}
+export default ComponentName;
